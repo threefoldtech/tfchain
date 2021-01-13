b585d22336400afaf1ceaf1976ec8f8b0778ebca
Muhamad Azamy <muhamad@incubaid.com>
2021-01-13 09:49:43 +0100

    Add subclient

---
+++ subclient/src/main.rs
@@ -0,0 +1,24 @@
+// builds with 1.48
+use anyhow::{Context, Result};
+use sp_keyring::AccountKeyring;
+use substrate_subxt::{extrinsic, ClientBuilder, NodeTemplateRuntime, PairSigner, RuntimeVersion};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    //println!("Hello, world!");
+
+    //let signer = PairSigner::new(AccountKeyring::Alice.pair());
+
+    let client = ClientBuilder::<NodeTemplateRuntime>::new()
+        .set_url("ws://127.0.0.1:9944")
+        .build()
+        .await
+        .context("failed to build client")?;
+
+    //client.create_twin();
+    //extrinsic::create_signed(c, genesis_hash: T::Hash, nonce: T::Index, call: Encoded, signer: &(dyn Signer<T> + Send + Sync))
+    let genesis = client.metadata();
+    println!("{}", genesis.pretty());
+
+    Ok(())
+}
