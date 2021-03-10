d3e093c09483769c6041411e286527a94023b452
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-03-10 14:57:20 +0100

    feat: make graphql 3.0 compatible

--- graphql/generated/graphql-server/db/migrations/commentSearch.migration.ts
+++ graphql/generated/graphql-server/db/migrations/commentSearch.migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CommentSearchMigration1615371859940 implements MigrationInterface {
-    name = 'commentSearchMigration1615371859940'
+export class CommentSearchMigration1615380681207 implements MigrationInterface {
+    name = 'commentSearchMigration1615380681207'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // TODO: escape 
--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -95,10 +95,10 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("substrate"),
+	impl_name: create_runtime_str!("substrate"),
 	authoring_version: 1,
-	spec_version: 3,
+	spec_version: 1,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
