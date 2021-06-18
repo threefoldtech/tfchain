3cc99e91243a33b7cef84297096b210f457e7186
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-06-18 10:53:22 +0200

    align graphql with devnet env

--- graphql/generated/graphql-server/db/migrations/commentSearch.migration.ts
+++ graphql/generated/graphql-server/db/migrations/commentSearch.migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CommentSearchMigration1615380681207 implements MigrationInterface {
-    name = 'commentSearchMigration1615380681207'
+export class CommentSearchMigration1624006231466 implements MigrationInterface {
+    name = 'commentSearchMigration1624006231466'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // TODO: escape 
