7475c235e3b9b3cbbe051890c26641bf55a0f667
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-08-02 14:44:51 +0200

    update cargo lock and gitpod

--- ct_scripts/install_rust.sh
+++ ct_scripts/install_rust.sh
@@ -4,9 +4,8 @@ set -ex
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$PATH:$HOME/.cargo/bin"
-rustup install nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup default stable
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
 # cargo build "--release"
-
-
