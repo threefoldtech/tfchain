9aa9426c3852817c9c7c3f3ab69989fcb921dbd4
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-10-15 11:40:13 +0200

    chore: update smart-contract dep and bump runtime and chart

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -106,7 +106,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("substrate-threefold"),
 	impl_name: create_runtime_str!("substrate-threefold"),
 	authoring_version: 1,
-	spec_version: 8,
+	spec_version: 9,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
