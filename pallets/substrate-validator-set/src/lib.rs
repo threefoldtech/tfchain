0d2d625849601bb3e32725183598bc6048a2953a
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-02-14 11:20:10 +0100

    chore: reset source substrate validator set to development branch

--- pallets/substrate-validator-set/src/lib.rs
+++ pallets/substrate-validator-set/src/lib.rs
@@ -8,23 +8,17 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::traits::{ChangeMembers, InitializeMembers};
-use pallet_session::Module as Session;
-use sp_runtime::traits::Convert;
 use sp_std::prelude::*;
+use sp_runtime::traits::Convert;
+use pallet_session::{Module as Session};
 
 pub use pallet::*;
 
-mod types;
-
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
-	use frame_support::{
-		dispatch::{DispatchError, DispatchErrorWithPostInfo, DispatchResultWithPostInfo},
-		pallet_prelude::*,
-	};
+	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use super::*;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -33,10 +27,7 @@ pub mod pallet {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
 		/// Origin for adding or removing a validator.
-		type AddRemoveOrigin: EnsureOrigin<<Self as frame_system::Config>::Origin>;
-
-		/// The receiver of the signal for when the membership has changed.
-		type MembershipChanged: ChangeMembers<Self::AccountId>;
+		type AddRemoveOrigin: EnsureOrigin<Self::Origin>;
 	}
 
 	#[pallet::pallet]
@@ -46,21 +37,11 @@ pub mod pallet {
 	// The pallet's runtime storage items.
 	#[pallet::storage]
 	#[pallet::getter(fn validators)]
-	pub type Validators<T: Config> = StorageValue<_, Vec<T::AccountId>>;
+	pub type Validators<T: Config> =  StorageValue<_, Vec<T::AccountId>>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn flag)]
-	pub type Flag<T: Config> = StorageValue<_, bool>;
-
-	#[pallet::storage]
-	#[pallet::getter(fn validator_requests)]
-	pub type ValidatorRequest<T: Config> =
-		StorageMap<_, Identity, u32, types::ValidatorRequest<T::AccountId>>;
-
-	/// Proposals so far.
-	#[pallet::storage]
-	#[pallet::getter(fn request_count)]
-	pub type RequestCount<T: Config> = StorageValue<_, u32, ValueQuery>;
+	pub type Flag<T: Config> =  StorageValue<_, bool>;
 
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
@@ -71,23 +52,12 @@ pub mod pallet {
 
 		// Validator removed.
 		ValidatorRemoved(T::AccountId),
-
-		StashAccountBound(u32, T::AccountId),
-		// Validator requests
-		ValidatorRequestCreated(types::ValidatorRequest<T::AccountId>),
-		ValidatorRequestApproved(types::ValidatorRequest<T::AccountId>),
-		ValidatorRequestExecuted(types::ValidatorRequest<T::AccountId>),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		NoValidators,
-		DuplicateValidatorRequest,
-		UnauthorizedToActivateValidator,
-		ValidatorRequestNotFound,
-		ValidatorRequestNotApproved,
-		StashAccountNotBound,
 	}
 
 	#[pallet::hooks]
@@ -101,9 +71,7 @@ pub mod pallet {
 	#[cfg(feature = "std")]
 	impl<T: Config> Default for GenesisConfig<T> {
 		fn default() -> Self {
-			Self {
-				validators: Vec::new(),
-			}
+			Self { validators: Vec::new() }
 		}
 	}
 
@@ -115,32 +83,36 @@ pub mod pallet {
 	}
 
 	#[pallet::call]
-	impl<T: Config> Pallet<T> {
+	impl<T:Config> Pallet<T> {
 		/// Add a new validator using root/sudo privileges.
 		///
 		/// New validator's session keys should be set in session module before calling this.
 		#[pallet::weight(0)]
-		pub fn add_validator(
-			origin: OriginFor<T>,
-			validator_id: T::AccountId,
-		) -> DispatchResultWithPostInfo {
+		pub fn add_validator(origin: OriginFor<T>, validator_id: T::AccountId) -> DispatchResultWithPostInfo {
 			T::AddRemoveOrigin::ensure_origin(origin)?;
 
-			Self::do_add(validator_id.clone());
+			let mut validators: Vec<T::AccountId>;
+
+			if <Validators<T>>::get().is_none() {
+				validators = vec![validator_id.clone()];
+			} else {
+				validators = <Validators<T>>::get().unwrap();
+				validators.push(validator_id.clone());
+			}
+
+			<Validators<T>>::put(validators);
+
 			// Calling rotate_session to queue the new session keys.
 			Session::<T>::rotate_session();
+			Self::deposit_event(Event::ValidatorAdded(validator_id));
 			// Triggering rotate session again for the queued keys to take effect.
 			Flag::<T>::put(true);
-
 			Ok(().into())
 		}
 
 		/// Remove a validator using root/sudo privileges.
 		#[pallet::weight(0)]
-		pub fn remove_validator(
-			origin: OriginFor<T>,
-			validator_id: T::AccountId,
-		) -> DispatchResultWithPostInfo {
+		pub fn remove_validator(origin: OriginFor<T>, validator_id: T::AccountId) -> DispatchResultWithPostInfo {
 			T::AddRemoveOrigin::ensure_origin(origin)?;
 			let mut validators = <Validators<T>>::get().ok_or(Error::<T>::NoValidators)?;
 
@@ -169,196 +141,15 @@ pub mod pallet {
 			Flag::<T>::put(true);
 			Ok(().into())
 		}
-
-		// Activate Validator takes in a validator request ID
-		// Based on that ID, it can fetch the validator request from storage
-		// check if the signer is mentioned as the council address from the request (this way we know if the signer is an active council member)
-		// if true, then we add the validator account from the request to the list of validators
-		#[pallet::weight(0)]
-		pub fn activate_validator(
-			origin: OriginFor<T>,
-			validator_request_id: u32,
-		) -> DispatchResultWithPostInfo {
-			let address = ensure_signed(origin)?;
-
-			let mut validator_request = <ValidatorRequest<T>>::get(validator_request_id)
-				.ok_or(DispatchError::from(Error::<T>::ValidatorRequestNotFound))?;
-
-			ensure!(
-				validator_request.state == types::ValidatorRequestState::Approved,
-				Error::<T>::ValidatorRequestNotApproved
-			);
-			ensure!(
-				validator_request.council_account == address,
-				Error::<T>::UnauthorizedToActivateValidator
-			);
-			ensure!(
-				validator_request.stash_account != T::AccountId::default(),
-				Error::<T>::StashAccountNotBound
-			);
-
-			// Update the validator request
-			validator_request.state = types::ValidatorRequestState::Executed;
-			<ValidatorRequest<T>>::insert(validator_request_id, &validator_request);
-
-			Self::do_add(validator_request.validator_account);
-			// Calling rotate_session to queue the new session keys.
-			Session::<T>::rotate_session();
-			// Triggering rotate session again for the queued keys to take effect.
-			Flag::<T>::put(true);
-
-			Ok(().into())
-		}
-
-		// create a request to become a validator
-		// Validator account: the account of the validator
-		// Stash account: the "bank" account of the validator (where rewards should be sent to)
-		// Description: why someone wants to become a validator
-		// Tf Connect ID: the threefold connect ID of the persion who wants to become a validator
-		// Info: some public info about the validator (website link, blog link, ..)
-		// A user can only have 1 validator request at a time
-		#[pallet::weight(0)]
-		pub fn create_validator_request(
-			origin: OriginFor<T>,
-			validator_account: T::AccountId,
-			description: Vec<u8>,
-			tf_connect_id: u64,
-			info: Vec<u8>,
-		) -> DispatchResultWithPostInfo {
-			let address = ensure_signed(origin.clone())?;
-
-			let validator_requests = <ValidatorRequest<T>>::iter();
-			for (_, req) in validator_requests {
-				if req.council_account == address {
-					return Err(DispatchErrorWithPostInfo::from(
-						Error::<T>::DuplicateValidatorRequest,
-					));
-				}
-			}
-
-			let id = Self::request_count();
-			let request = types::ValidatorRequest {
-				id,
-				council_account: address,
-				validator_account,
-				stash_account: T::AccountId::default(),
-				description,
-				tf_connect_id,
-				info,
-				state: types::ValidatorRequestState::Created,
-			};
-
-			<RequestCount<T>>::mutate(|i| *i += 1);
-
-			// Create a validator request object
-			<ValidatorRequest<T>>::insert(id, &request);
-			Self::deposit_event(Event::ValidatorRequestCreated(request.clone()));
-
-			Ok(().into())
-		}
-
-		// Bind a stash account to a validator request
-		// This needs to be called before a user can call activate_validator
-		#[pallet::weight(0)]
-		pub fn bind_stash_account(
-			origin: OriginFor<T>,
-			validator_request_id: u32,
-		) -> DispatchResultWithPostInfo {
-			let address = ensure_signed(origin.clone())?;
-
-			// TODO check if address has X enough balance?
-
-			let mut validator_request = <ValidatorRequest<T>>::get(validator_request_id)
-				.ok_or(DispatchError::from(Error::<T>::ValidatorRequestNotFound))?;
-			validator_request.stash_account = address.clone();
-
-			<ValidatorRequest<T>>::insert(validator_request_id, validator_request);
-
-			Self::deposit_event(Event::StashAccountBound(validator_request_id, address.clone()));
-
-			Ok(().into())
-		}
-
-		// Approve a validator request by ID
-		#[pallet::weight(0)]
-		pub fn approve_validator_request(
-			origin: OriginFor<T>,
-			validator_request_id: u32,
-		) -> DispatchResultWithPostInfo {
-			T::AddRemoveOrigin::ensure_origin(origin)?;
-
-			let validator_requests = <ValidatorRequest<T>>::iter();
-			for (id, mut req) in validator_requests {
-				if req.id == validator_request_id {
-					req.state = types::ValidatorRequestState::Approved;
-					<ValidatorRequest<T>>::insert(id, &req);
-					Self::deposit_event(Event::ValidatorRequestApproved(req));
-				}
-			}
-
-			Ok(().into())
-		}
-	}
-}
-
-impl<T: Config> Pallet<T> {
-	fn do_add(validator_id: T::AccountId) {
-		let mut validators: Vec<T::AccountId>;
-
-		if <Validators<T>>::get().is_none() {
-			validators = vec![validator_id.clone()];
-		} else {
-			validators = <Validators<T>>::get().unwrap();
-			// insert the validator if it does not exist yet in the list
-			if !validators.contains(&validator_id.clone()) {
-				validators.push(validator_id.clone());
-				Self::deposit_event(Event::ValidatorAdded(validator_id));
-			}
-		}
-
-		<Validators<T>>::put(validators);
-	}
-
-	fn do_approve_request(council_address: T::AccountId) {
-		let validator_requests = <ValidatorRequest<T>>::iter();
-		for (id, mut req) in validator_requests {
-			if req.council_account == council_address {
-				req.state = types::ValidatorRequestState::Approved;
-				<ValidatorRequest<T>>::insert(id, &req);
-				Self::deposit_event(Event::ValidatorRequestApproved(req));
-			}
-		}
-	}
-}
-
-impl<T: Config> ChangeMembers<T::AccountId> for Pallet<T> {
-	fn change_members_sorted(
-		_incoming: &[T::AccountId],
-		_outgoing: &[T::AccountId],
-		new: &[T::AccountId],
-	) {
-		for council_member in new {
-			Self::do_approve_request(council_member.clone());
-		}
-	}
-}
-
-impl<T: Config> InitializeMembers<T::AccountId> for Pallet<T> {
-	fn initialize_members(init: &[T::AccountId]) {
-		<Validators<T>>::put(init);
-		// Shouldn't need a flag update here as this should happen at genesis
 	}
 }
 
 impl<T: Config> Pallet<T> {
 	fn initialize_validators(validators: &[T::AccountId]) {
-		if !validators.is_empty() {
-			assert!(
-				<Validators<T>>::get().is_none(),
-				"Validators are already initialized!"
-			);
-			<Validators<T>>::put(validators);
-		}
+			if !validators.is_empty() {
+				assert!(<Validators<T>>::get().is_none(), "Validators are already initialized!");
+				<Validators<T>>::put(validators);
+			}
 	}
 }
 
