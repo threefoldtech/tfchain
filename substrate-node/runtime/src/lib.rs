a36d28162a6b6e085d10d15f57d748938ea9b79c
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-06-14 11:42:41 +0200

    chore: lower dao motionduration to 2 hours #333

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -512,7 +512,7 @@ impl pallet_scheduler::Config for Runtime {
 }
 
 parameter_types! {
-    pub const CouncilMotionDuration: BlockNumber = 7 * DAYS;
+    pub const CouncilMotionDuration: BlockNumber = 2 * HOURS;
     pub const CouncilMaxProposals: u32 = 100;
     pub const CouncilMaxMembers: u32 = 100;
 }
