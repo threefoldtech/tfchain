f0adba3410b14e8fda6441410fe1bb2fadf14716
dylanVerstraete <dylan_verstraete1@hotmail.com>
2022-06-29 17:23:29 +0200

    fix: ipv6 parsing

--- substrate-node/pallets/pallet-tfgrid/src/ipv6.rs
+++ substrate-node/pallets/pallet-tfgrid/src/ipv6.rs
@@ -1,4 +1,12 @@
 pub fn valid_ipv6(input: &[u8]) -> bool {
+    if input.len() < 3 {
+        return false;
+    }
+    // Trap invalid input with leading single : so we don't have to track that in the loop.
+    // Loop later verifies that the second char is actually a valid input char.
+    if input[0] == b':' && input[1] != b':' {
+        return false;
+    }
     let mut current_group_elements = 0;
     let mut sections = 1;
     let mut double_colon_seen = false;
@@ -10,6 +18,9 @@ pub fn valid_ipv6(input: &[u8]) -> bool {
                 colons = 0;
             }
             b':' => {
+                // Unconditionally increment section count. This will add 2 sections if :: is
+                // detected, but that is fine since :: is only allowed if it masks at least 1 all 0
+                // section.
                 colons += 1;
                 sections += 1;
                 current_group_elements = 0;
@@ -28,23 +39,33 @@ pub fn valid_ipv6(input: &[u8]) -> bool {
         }
     }
 
-    true
+    // Ending in a colon is not allowed.
+    colons == 0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::valid_ipv6;
-
     #[test]
     fn it_works() {
-        assert!(valid_ipv6(b"::1"));
-        assert!(valid_ipv6(b"200::1"));
-        assert!(valid_ipv6(b"200:3412:023::22:1"));
-        assert!(valid_ipv6(b"dead:babe:dead::babe:dead"));
-        assert!(valid_ipv6(b"dead:babe:dead:babe:dead:babe:dead:babe"));
-        assert!(valid_ipv6(b"ffff:ff:ff::ac:0000:fffe"));
-        assert!(!valid_ipv6(b"fffg:ff:ff::ac:0000:fffe"));
-        assert!(!valid_ipv6(b"fffg:::ac:0000:fffe"));
-        assert!(!valid_ipv6(b"fffg::d::ac:0000:fffe"));
+        assert!(super::valid_ipv6(b"::1"));
+        assert!(!super::valid_ipv6(b":1"));
+        assert!(super::valid_ipv6(b"200::1"));
+        assert!(super::valid_ipv6(b"200:3412:023::22:1"));
+        assert!(super::valid_ipv6(b"dead:babe:dead::babe:dead"));
+        assert!(super::valid_ipv6(
+            b"dead:babe:dead:babe:dead:babe:dead:babe"
+        ));
+        assert!(super::valid_ipv6(b"ffff:ff:ff::ac:0000:fffe"));
+        assert!(!super::valid_ipv6(
+            b"dead:babe:dead:babe:dead:babe:dead:babe:dead"
+        ));
+        assert!(!super::valid_ipv6(
+            b"dead:babe:dead::babe:dead:babe:dead:babe"
+        ));
+        assert!(!super::valid_ipv6(b"fffg:ff:ff::ac:0000:fffe"));
+        assert!(!super::valid_ipv6(b"ffff:::ac:0000:fffe"));
+        assert!(!super::valid_ipv6(b"ffff::d::ac:0000:fffe"));
+        assert!(!super::valid_ipv6(b"ffff::"));
+        assert!(!super::valid_ipv6(b"ffff:"));
     }
 }
