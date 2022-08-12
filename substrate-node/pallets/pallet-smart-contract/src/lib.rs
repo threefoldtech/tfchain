9b8b2797a56747d6d63a497dc608e3dc0a2c701b
brandonpille <brandon@threefold.tech>
2022-08-12 16:42:28 +0200

    FEAT: removed unused imports #269

--- substrate-node/pallets/pallet-smart-contract/src/lib.rs
+++ substrate-node/pallets/pallet-smart-contract/src/lib.rs
@@ -26,11 +26,10 @@ use pallet_tfgrid::pallet::{InterfaceOf, LocationOf, PubConfigOf, SerialNumberOf
 use pallet_tfgrid::types as pallet_tfgrid_types;
 use pallet_timestamp as timestamp;
 use sp_core::crypto::KeyTypeId;
-use sp_runtime::traits::BlockNumberProvider;
 use sp_runtime::{
     offchain::{
         storage::StorageValueRef,
-        storage_lock::{BlockAndTime, StorageLock, Time},
+        storage_lock::{BlockAndTime, StorageLock},
         Duration,
     },
     traits::{CheckedSub, SaturatedConversion},
