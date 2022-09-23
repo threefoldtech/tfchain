f97f6b30ab29931dd4d341660e4405e267fc77b7
brandonpille <brandon@threefold.tech>
2022-09-23 11:06:05 +0200

    feat: fixed rebase conflict #440

--- substrate-node/pallets/pallet-dao/src/lib.rs
+++ substrate-node/pallets/pallet-dao/src/lib.rs
@@ -223,16 +223,6 @@ pub mod pallet {
                 end = now + motion_duration;
             }
 
-            let now = frame_system::Pallet::<T>::block_number();
-            let mut end = now + T::MotionDuration::get();
-            if let Some(motion_duration) = duration {
-                ensure!(
-                    motion_duration < T::BlockNumber::from(constants::time::DAYS * 30),
-                    Error::<T>::InvalidProposalDuration
-                );
-                end = now + motion_duration;
-            }
-
             let index = Self::proposal_count();
             <ProposalCount<T>>::mutate(|i| *i += 1);
             <ProposalOf<T>>::insert(proposal_hash, *action);
