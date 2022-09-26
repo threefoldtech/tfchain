38d57a5c160d8c1fa3b5259f4d97b5a339ffca5f
dylanVerstraete <dylan_verstraete1@hotmail.com>
2022-09-26 16:01:25 +0200

    chore: reset burn transation id

--- substrate-node/runtime/src/bridge_migration.rs
+++ substrate-node/runtime/src/bridge_migration.rs
@@ -3,8 +3,7 @@ use super::*;
 pub struct RemoveBridgeStorage;
 impl frame_support::traits::OnRuntimeUpgrade for RemoveBridgeStorage {
     fn on_runtime_upgrade() -> Weight {
-        use frame_support::storage::migration;
-
+        frame_support::log::info!("🔍️ RemoveBridgeStorage start");
         let limit = None; // delete all values
         pallet_tft_bridge::MintTransactions::<Runtime>::remove_all(limit);
         pallet_tft_bridge::ExecutedMintTransactions::<Runtime>::remove_all(limit);
@@ -12,7 +11,9 @@ impl frame_support::traits::OnRuntimeUpgrade for RemoveBridgeStorage {
         pallet_tft_bridge::ExecutedBurnTransactions::<Runtime>::remove_all(limit);
         pallet_tft_bridge::RefundTransactions::<Runtime>::remove_all(limit);
         pallet_tft_bridge::ExecutedRefundTransactions::<Runtime>::remove_all(limit);
+        pallet_tft_bridge::BurnTransactionID::<Runtime>::put(0);
 
+        frame_support::log::info!("RemoveBridgeStorage Done");
         <Runtime as frame_system::Config>::DbWeight::get().writes(6)
     }
 }
