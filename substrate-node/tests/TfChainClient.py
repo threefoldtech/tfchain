6a5b9907c9a9930ff7d0f2d76e3b0293a5339f10
brandonpille <brandon@threefold.tech>
2022-11-03 13:12:52 +0100

    added change_power_state in integration tests

--- substrate-node/tests/TfChainClient.py
+++ substrate-node/tests/TfChainClient.py
@@ -33,7 +33,7 @@ UNIT_TERRABYTES = "Terrabytes"
 UNIT_TYPES = [UNIT_BYTES, UNIT_KILOBYTES,
               UNIT_MEGABYTES, UNIT_GIGABYTES, UNIT_TERRABYTES]
 
-EMPTY_RESOURCES = { "hru": 0, "mru": 0, "cru": 0, "sru": 0}
+EMPTY_RESOURCES = {"hru": 0, "mru": 0, "cru": 0, "sru": 0}
 
 
 class TfChainClient:
@@ -92,6 +92,21 @@ class TfChainClient:
         self._check_events([event.value["event"]
                            for event in response.triggered_events], expected_events)
 
+    def get_account_name_from_twin_id(self, twin_id: int = 1, port: int = DEFAULT_PORT):
+        if twin_id == self.get_twin_id(port=port, who="Alice"):
+            return "Alice"
+        elif twin_id == self.get_twin_id(port=port, who="Bob"):
+            return "Bob"
+        elif twin_id == self.get_twin_id(port=port, who="Charlie"):
+            return "Charlie"
+        elif twin_id == self.get_twin_id(port=port, who="Dave"):
+            return "Dave"
+        elif twin_id == self.get_twin_id(port=port, who="Eve"):
+            return "Eve"
+        elif twin_id == self.get_twin_id(port=port, who="Ferdie"):
+            return "Ferdie"
+        return None
+
     def setup_predefined_account(self, who: str, port: int = DEFAULT_PORT):
         logging.info("Setting up predefined account %s (%s)", who,
                      PREDEFINED_KEYS[who].ss58_address)
@@ -107,6 +122,14 @@ class TfChainClient:
                                           "document_hash": "garbage"
                                       })
         self._sign_extrinsic_submit_check_response(substrate, call, who)
+
+    def get_twin_id(self, port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
+        substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
+
+        q = substrate.query("TfgridModule", "TwinIdByAccountID", [
+            PREDEFINED_KEYS[who].ss58_address])
+
+        return q.value
 
     def create_twin(self, ip: str = "::1", port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
         substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
@@ -307,6 +330,12 @@ class TfChainClient:
         self._sign_extrinsic_submit_check_response(
             substrate, call, who, expected_events=expected_events)
 
+        node_id = self.get_current_node_id(port=port)
+        nodes = self.get_nodes_from_farm(farm_id=farm_id, port=port)
+        if node_id != nodes[0]:
+            self.bring_node_down(
+                node_id=node_id, leader_node=nodes[0], port=port)
+
     def update_node(self, node_id: int = 1, farm_id: int = 1, resources: dict = EMPTY_RESOURCES,
                     longitude: str = "", latitude: str = "", country: str = "", city: str = "",
                     secure_boot: bool = False, virtualized: bool = False, serial_number: str = DEFAULT_SERIAL_NUMBER,
@@ -385,19 +414,80 @@ class TfChainClient:
 
         q = substrate.query("TfgridModule", "Nodes", [id])
         return q.value
-    
+
+    def get_current_node_id(self, port: int = DEFAULT_PORT):
+        substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
+
+        q = substrate.query("TfgridModule", "NodeID", [])
+        return q.value
+
     def get_contract(self, id: int = 1, port: int = DEFAULT_PORT):
         substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
 
         q = substrate.query("SmartContractModule", "Contracts", [id])
-        
+
+        return q.value
+
+    def get_current_contract_id(self, port: int = DEFAULT_PORT):
+        substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
+
+        q = substrate.query("SmartContractModule", "ContractID", [])
+
         return q.value
 
     def get_node_id_from_capacity_reservation_contract(self, contract_id: int = 1, port: int = DEFAULT_PORT):
         contract = self.get_contract(id=contract_id, port=port)
         assert contract is not None, "Contract doesn't exist"
-        assert "CapacityReservationContract" in contract["contract_type"], "Contract is not a Capacity Reservation Contract"
+        assert "CapacityReservationContract" in contract[
+            "contract_type"], "Contract is not a Capacity Reservation Contract"
         return contract["contract_type"]["CapacityReservationContract"]["node_id"]
+
+    def get_nodes_from_farm(self, farm_id: int = 1, port: int = DEFAULT_PORT):
+        substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
+
+        q = substrate.query("TfgridModule", "NodesByFarmID", [farm_id])
+
+        return q.value
+
+    def change_power_state(self, power_state: str | dict = "Up", port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
+        substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
+
+        call = substrate.compose_call("TfgridModule", "change_power_state",
+                                      {
+                                          "power_state": power_state
+                                      })
+        logging.info("Ok")
+        expected_events = [{
+            "module_id": "TfgridModule",
+            "event_id": "PowerStateChanged"
+        }]
+        self._sign_extrinsic_submit_check_response(
+            substrate, call, who, expected_events=expected_events)
+
+    def bring_node_up(self, node_id: int = 1, port: int = DEFAULT_PORT):
+        node = self.get_node(id=node_id, port=port)
+        logging.info("Node is %s", json.dumps(node))
+        if node["power"]["state"] == "Down":
+            owner_node = self.get_account_name_from_twin_id(
+                twin_id=node["twin_id"], port=port)
+            logging.info("Owner is %s", owner_node)
+            self.change_power_state(
+                power_state="Up", port=port, who=owner_node)
+            node = self.get_node(id=node_id, port=port)
+            assert node["power"]["state"] == "Up", f"Failed to bring node {node_id} up!"
+
+    def bring_node_down(self, node_id: int = 1, leader_node: int = 1, port: int = DEFAULT_PORT):
+        node = self.get_node(id=node_id, port=port)
+        logging.info("Node is %s", json.dumps(node))
+        if node["power"]["state"] == "Up":
+            owner_node = self.get_account_name_from_twin_id(
+                twin_id=node["twin_id"], port=port)
+            logging.info("Owner is %s", owner_node)
+            self.change_power_state(
+                power_state={"Down": leader_node}, port=port, who=owner_node)
+            node = self.get_node(id=node_id, port=port)
+            assert node["power"]["state"] == {
+                "Down": leader_node}, f"Failed to bring node {node_id} down: {json.dumps(node)}"
 
     def create_capacity_reservation_contract(self, farm_id: int = 1, policy: dict = {},
                                              solution_provider_id: int | None = None, port: int = DEFAULT_PORT,
@@ -420,16 +510,23 @@ class TfChainClient:
         self._sign_extrinsic_submit_check_response(
             substrate, call, who, expected_events=expected_events)
 
+        # bring node up if it is down
+        contract_id = self.get_current_contract_id(port=port)
+        logging.info("Contract id %s", contract_id)
+        node_id = self.get_node_id_from_capacity_reservation_contract(
+            contract_id=contract_id, port=port)
+        self.bring_node_up(node_id=node_id, port=port)
+
     def create_deployment_contract(self, capacity_reservation_id: int = 1, deployment_data: None | bytes = None,
-                                   deployment_hash: None | bytes = None, public_ips: int = 0, 
-                                   resources: dict = EMPTY_RESOURCES, contract_policy: int | None = None, 
+                                   deployment_hash: None | bytes = None, public_ips: int = 0,
+                                   resources: dict = EMPTY_RESOURCES, contract_policy: int | None = None,
                                    port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
         substrate = self._connect_to_server(f"ws://127.0.0.1:{port}")
         if deployment_data is None:
             deployment_data = randbytes(32)
         if deployment_hash is None:
             deployment_hash = randbytes(32)
-            
+
         params = {
             "capacity_reservation_id": capacity_reservation_id,
             "deployment_data": deployment_data,
@@ -502,8 +599,14 @@ class TfChainClient:
                               type="Deployment", port=port, who=who)
 
     def cancel_capacity_reservation_contract(self, contract_id: int = 1, port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
+        node_id = self.get_node_id_from_capacity_reservation_contract(
+            contract_id=contract_id, port=port)
         self._cancel_contract(contract_id=contract_id,
                               type="CapacityReservation", port=port, who=who)
+        node = self.get_node(id=node_id, port=port)
+        nodes = self.get_nodes_from_farm(farm_id=node["farm_id"], port=port)
+        if node_id != nodes[0] and node["resources"]["used_resources"]["hru"] == 0 and node["resources"]["used_resources"]["sru"] == 0 and node["resources"]["used_resources"]["cru"] == 0 and node["resources"]["used_resources"]["mru"] == 0:
+            self.bring_node_down(node_id, nodes[0], port=port)
 
     def add_nru_reports(self, contract_id: int = 1, nru: int = 1, port: int = DEFAULT_PORT, who: str = DEFAULT_SIGNER):
         block_number = self.get_block_number(port=port)
