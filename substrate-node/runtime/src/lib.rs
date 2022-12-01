f76bcb5be92b14ad082ffa5f3a46a683dddee255
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-12-01 10:53:33 +0100

    chore: reinstate older migrations

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -766,10 +766,8 @@ pub type Executive = frame_executive::Executive<
     AllPalletsWithSystem,
     (
         pallet_smart_contract::migrations::v6::ContractMigrationV5<Runtime>,
-        /*
         pallet_tfgrid::migrations::v10::FixFarmNodeIndexMap<Runtime>,
         pallet_tfgrid::migrations::v11::FixFarmingPolicy<Runtime>,
-        */
         pallet_tfgrid::migrations::v12::InputValidation<Runtime>,
         pallet_tfgrid::migrations::v13::NodeMigration<Runtime>,
         pallet_tfgrid::migrations::v14::FixPublicIP<Runtime>,
