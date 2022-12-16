18234bfe34d699a67c0e98b0201693205632d4b4
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-12-16 15:17:25 +0100

    chore: remove dead code

--- substrate-node/pallets/pallet-smart-contract/src/benchmarking.rs
+++ substrate-node/pallets/pallet-smart-contract/src/benchmarking.rs
@@ -1,23 +1,3 @@
-// This file is part of Substrate.
-
-// Copyright (C) 2022 Threefold Tech
-// Copyright (C) 2020-2021 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// 	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//! Vesting pallet benchmarking.
-
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
@@ -314,18 +294,3 @@ pub(crate) fn get_document_hash_input(document_hash_input: &[u8]) -> DocumentHas
 pub(crate) fn get_twin_ip_input(twin_ip_input: &[u8]) -> TwinIpInput {
     BoundedVec::try_from(twin_ip_input.to_vec()).expect("Invalid twin ip input.")
 }
-
-// pub fn run_to_block<T: Config>(n: u64)
-// where
-//     Timestamp::Config<T>::Moment: u64,
-// {
-//     Timestamp::Pallet::<T>::set_timestamp((1628082000 * 1000) + (6000 * n));
-//     while System::Pallet::<T>::block_number() <= n {
-//         SmartContractModule::<T>::offchain_worker(System::Pallet::<T>::block_number());
-//         SmartContractModule::<T>::on_finalize(System::Pallet::<T>::block_number());
-//         System::Pallet::<T>::on_finalize(System::Pallet::<T>::block_number());
-//         System::Pallet::<T>::set_block_number(System::Pallet::<T>::block_number() + 1);
-//         System::Pallet::<T>::on_initialize(System::Pallet::<T>::block_number());
-//         SmartContractModule::<T>::on_initialize(System::Pallet::<T>::block_number());
-//     }
-// }
