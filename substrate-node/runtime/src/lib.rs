ef3c64e7cf680e28e9995ce7b3c55641ca1a42ec
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-12-20 16:36:56 +0100

    chore: bump to version 2.2.0-rc6 (specVersion: 121)

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("substrate-threefold"),
     impl_name: create_runtime_str!("substrate-threefold"),
     authoring_version: 1,
-    spec_version: 120,
+    spec_version: 121,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
