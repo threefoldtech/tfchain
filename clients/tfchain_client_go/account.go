a3fdb3e08bde5fbfb015ceb468de20b195750aad
dylanverstraete <dylan_verstraete1@hotmail.com>
2023-04-03 12:00:38 +0200

    Revert "try remove go client"
    This reverts commit 2a51d7daecc111c25dc40da9af4b44804b89baad.

---
+++ clients/tfchain_client_go/account.go
@@ -0,0 +1,375 @@
+package substrate
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/signature"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/jbenet/go-base58"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/vedhavyas/go-subkey"
+	subkeyEd25519 "github.com/vedhavyas/go-subkey/ed25519"
+	subkeySr25519 "github.com/vedhavyas/go-subkey/sr25519"
+)
+
+const (
+	network = 42
+)
+
+// AccountID type
+type AccountID types.AccountID
+
+// Balance
+type Balance struct {
+	Free       types.U128
+	Reserved   types.U128
+	MiscFrozen types.U128
+	FreeFrozen types.U128
+}
+
+type AccountInfo struct {
+	Nonce       types.U32
+	Consumers   types.U32
+	Providers   types.U32
+	Sufficients types.U32
+	Data        struct {
+		Free       types.U128
+		Reserved   types.U128
+		MiscFrozen types.U128
+		FreeFrozen types.U128
+	}
+}
+
+// PublicKey gets public key from account id
+func (a AccountID) PublicKey() []byte {
+	return a[:]
+}
+
+// String return string representation of account
+func (a AccountID) String() string {
+	address, _ := subkey.SS58Address(a[:], network)
+	return address
+}
+
+// MarshalJSON implementation
+func (a AccountID) MarshalJSON() ([]byte, error) {
+	address, err := subkey.SS58Address(a[:], network)
+	if err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(address)
+}
+
+// FromAddress creates an AccountID from a SS58 address
+func FromAddress(address string) (account AccountID, err error) {
+	bytes := base58.Decode(address)
+	if len(bytes) != 3+len(account) {
+		return account, fmt.Errorf("invalid address length")
+	}
+	if bytes[0] != network {
+		return account, fmt.Errorf("invalid address format")
+	}
+
+	copy(account[:], bytes[1:len(account)+1])
+	return
+}
+
+func FromKeyBytes(address []byte) (string, error) {
+	return subkey.SS58Address(address, network)
+}
+
+// keyringPairFromSecret creates KeyPair based on seed/phrase and network
+// Leave network empty for default behavior
+func keyringPairFromSecret(seedOrPhrase string, network uint8, scheme subkey.Scheme) (signature.KeyringPair, error) {
+	kyr, err := subkey.DeriveKeyPair(scheme, seedOrPhrase)
+
+	if err != nil {
+		return signature.KeyringPair{}, err
+	}
+
+	ss58Address, err := kyr.SS58Address(network)
+	if err != nil {
+		return signature.KeyringPair{}, err
+	}
+
+	var pk = kyr.Public()
+
+	return signature.KeyringPair{
+		URI:       seedOrPhrase,
+		Address:   ss58Address,
+		PublicKey: pk,
+	}, nil
+}
+
+var (
+	ErrAccountNotFound = fmt.Errorf("account not found")
+)
+
+/*
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"kycSignature": "", "data": {"name": "", "email": ""}, "substrateAccountID": "5DAprR72N6s7AWGwN7TzV9MyuyGk9ifrq8kVxoXG9EYWpic4"}' \
+  https://api.substrate01.threefold.io/activate
+*/
+
+func (s *Substrate) activateAccount(identity Identity, activationURL string) error {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(map[string]string{
+		"substrateAccountID": identity.Address(),
+	}); err != nil {
+		return errors.Wrap(err, "failed to build required body")
+	}
+
+	response, err := http.Post(activationURL, "application/json", &buf)
+	if err != nil {
+		return errors.Wrap(err, "failed to call activation service")
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK || response.StatusCode == http.StatusConflict {
+		// it went fine.
+		return nil
+	}
+
+	return fmt.Errorf("failed to activate account: %s", response.Status)
+}
+
+// EnsureAccount makes sure account is available on blockchain
+// if not, it uses activation service to create one
+func (s *Substrate) EnsureAccount(identity Identity, activationURL, termsAndConditionsLink, terminsAndConditionsHash string) (info AccountInfo, err error) {
+	cl, meta, err := s.getClient()
+	if err != nil {
+		return info, err
+	}
+	info, err = s.getAccount(cl, meta, identity)
+	if errors.Is(err, ErrAccountNotFound) {
+		// account activation
+		log.Debug().Msg("account not found ... activating")
+		if err = s.activateAccount(identity, activationURL); err != nil {
+			return
+		}
+
+		// after activation this can take up to 10 seconds
+		// before the account is actually there !
+
+		exp := backoff.NewExponentialBackOff()
+		exp.MaxElapsedTime = 10 * time.Second
+		exp.MaxInterval = 3 * time.Second
+
+		err = backoff.Retry(func() error {
+			info, err = s.getAccount(cl, meta, identity)
+			return err
+		}, exp)
+	}
+
+	log.Info().Str("address", identity.Address()).Msg("account")
+	account, err := FromAddress(identity.Address())
+	if err != nil {
+		return info, errors.Wrap(err, "failed to get account id for identity")
+	}
+
+	conditions, err := s.SignedTermsAndConditions(account)
+	if err != nil {
+		return info, err
+	}
+
+	if len(conditions) > 0 {
+		return info, nil
+	}
+
+	return info, s.AcceptTermsAndConditions(identity, termsAndConditionsLink, terminsAndConditionsHash)
+}
+
+// Identity is a user identity
+type Identity interface {
+	KeyPair() (subkey.KeyPair, error)
+	Sign(data []byte) ([]byte, error)
+	Type() string
+	MultiSignature(sig []byte) types.MultiSignature
+	Address() string
+	PublicKey() []byte
+	URI() string
+}
+
+type srIdentity struct {
+	signature.KeyringPair
+}
+type edIdentity struct {
+	signature.KeyringPair
+}
+
+func NewIdentityFromEd25519Key(sk ed25519.PrivateKey) (Identity, error) {
+	str := types.HexEncodeToString(sk.Seed())
+	krp, err := keyringPairFromSecret(str, network, subkeyEd25519.Scheme{})
+	if err != nil {
+		return nil, err
+	}
+	return &edIdentity{krp}, nil
+}
+
+func NewIdentityFromEd25519Phrase(phrase string) (Identity, error) {
+	krp, err := keyringPairFromSecret(phrase, network, subkeyEd25519.Scheme{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &edIdentity{krp}, nil
+}
+
+func (i *edIdentity) KeyPair() (subkey.KeyPair, error) {
+	kyr, err := subkey.DeriveKeyPair(subkeyEd25519.Scheme{}, i.KeyringPair.URI)
+	if err != nil {
+		return nil, err
+	}
+	return kyr, nil
+}
+
+func (i edIdentity) Address() string {
+	return i.KeyringPair.Address
+}
+
+func (i edIdentity) URI() string {
+	return i.KeyringPair.URI
+}
+
+func (i edIdentity) PublicKey() []byte {
+	return i.KeyringPair.PublicKey
+}
+
+func (i edIdentity) Sign(data []byte) ([]byte, error) {
+	return signBytes(data, i.KeyringPair.URI, subkeyEd25519.Scheme{})
+}
+
+func (i edIdentity) MultiSignature(sig []byte) types.MultiSignature {
+	return types.MultiSignature{IsEd25519: true, AsEd25519: types.NewSignature(sig)}
+}
+
+func (i edIdentity) Type() string {
+	return "ed25519"
+}
+
+func NewIdentityFromSr25519Phrase(phrase string) (Identity, error) {
+	krp, err := keyringPairFromSecret(phrase, network, subkeySr25519.Scheme{})
+	if err != nil {
+		return nil, err
+	}
+	return &srIdentity{krp}, nil
+}
+
+func (i *srIdentity) KeyPair() (subkey.KeyPair, error) {
+	kyr, err := subkey.DeriveKeyPair(subkeySr25519.Scheme{}, i.KeyringPair.URI)
+	if err != nil {
+		return nil, err
+	}
+	return kyr, nil
+}
+
+func (i srIdentity) Address() string {
+	return i.KeyringPair.Address
+}
+
+func (i srIdentity) URI() string {
+	return i.KeyringPair.URI
+}
+
+func (i srIdentity) PublicKey() []byte {
+	return i.KeyringPair.PublicKey
+}
+
+func (i srIdentity) Sign(data []byte) ([]byte, error) {
+	return signBytes(data, i.KeyringPair.URI, subkeySr25519.Scheme{})
+}
+
+func (i srIdentity) MultiSignature(sig []byte) types.MultiSignature {
+	return types.MultiSignature{IsSr25519: true, AsSr25519: types.NewSignature(sig)}
+}
+
+func (i srIdentity) Type() string {
+	return "sr25519"
+}
+
+func (s *Substrate) getAccount(cl Conn, meta Meta, identity Identity) (info AccountInfo, err error) {
+	key, err := types.CreateStorageKey(meta, "System", "Account", identity.PublicKey(), nil)
+	if err != nil {
+		err = errors.Wrap(err, "failed to create storage key")
+		return
+	}
+
+	ok, err := cl.RPC.State.GetStorageLatest(key, &info)
+	if err != nil || !ok {
+		if !ok {
+			return info, ErrAccountNotFound
+		}
+
+		return
+	}
+
+	return
+}
+
+// GetAccount gets account info with secure key
+func (s *Substrate) GetAccount(identity Identity) (info AccountInfo, err error) {
+	cl, meta, err := s.getClient()
+	if err != nil {
+		return info, err
+	}
+
+	return s.getAccount(cl, meta, identity)
+}
+
+// GetAccountPublicInfo gets the info for a given account ID
+func (s *Substrate) GetAccountPublicInfo(account AccountID) (info AccountInfo, err error) {
+	cl, meta, err := s.getClient()
+	if err != nil {
+		return
+	}
+
+	key, err := types.CreateStorageKey(meta, "System", "Account", account.PublicKey(), nil)
+	if err != nil {
+		return info, errors.Wrap(err, "failed to create substrate query key")
+	}
+
+	ok, err := cl.RPC.State.GetStorageLatest(key, &info)
+	if err != nil || !ok {
+		if !ok {
+			return info, ErrAccountNotFound
+		}
+		return
+	}
+
+	return
+}
+
+// GetBalance gets the balance for a given account ID
+func (s *Substrate) GetBalance(account AccountID) (balance Balance, err error) {
+	cl, meta, err := s.getClient()
+	if err != nil {
+		return
+	}
+
+	key, err := types.CreateStorageKey(meta, "System", "Account", account[:], nil)
+	if err != nil {
+		return balance, errors.Wrap(err, "failed to create substrate query key")
+	}
+
+	var info AccountInfo
+	ok, err := cl.RPC.State.GetStorageLatest(key, &info)
+	balance = info.Data
+	if err != nil || !ok {
+		if !ok {
+			return balance, ErrAccountNotFound
+		}
+		return
+	}
+
+	return
+}
---
+++ clients/tfchain_client_go/utils.go
@@ -0,0 +1,499 @@
+package substrate
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/vedhavyas/go-subkey"
+	"golang.org/x/crypto/blake2b"
+)
+
+var (
+	ErrIsUsurped = fmt.Errorf("is usurped")
+	Gigabyte     = 1024 * 1024 * 1024
+)
+
+// map from module index to error list
+// https://github.com/threefoldtech/tfchain/blob/development/substrate-node/runtime/src/lib.rs#L701
+var moduleErrors = [][]string{
+	nil,                       // System
+	nil,                       // RandomnessCollectiveFlip
+	nil,                       // Timestamp
+	nil,                       // Balances
+	nil,                       // ValidatorSet
+	nil,                       // Session
+	nil,                       // Aura
+	nil,                       // Grandpa
+	nil,                       // TransactionPayment
+	nil,                       // Sudo
+	nil,                       // Authorship
+	tfgridModuleErrors,        // TfgridModule
+	smartContractModuleErrors, // SmartContractModule
+	tftBridgeModuleErrors,     // TFTBridgeModule
+	nil,                       // TFTPriceModule
+	nil,                       // Scheduler
+	nil,                       // BurningModule
+	nil,                       // TFKVStore
+	nil,                       // Council
+	nil,                       // CouncilMembership
+	nil,                       // RuntimeUpgrade
+	nil,                       // Validator
+	nil,                       // Dao
+	nil,                       // Utility
+}
+
+// https://github.com/threefoldtech/tfchain_pallets/blob/bc9c5d322463aaf735212e428da4ea32b117dc24/pallet-smart-contract/src/lib.rs#L58
+var smartContractModuleErrors = []string{
+	"TwinNotExists",
+	"NodeNotExists",
+	"FarmNotExists",
+	"FarmHasNotEnoughPublicIPs",
+	"FarmHasNotEnoughPublicIPsFree",
+	"FailedToReserveIP",
+	"FailedToFreeIPs",
+	"ContractNotExists",
+	"TwinNotAuthorizedToUpdateContract",
+	"TwinNotAuthorizedToCancelContract",
+	"NodeNotAuthorizedToDeployContract",
+	"NodeNotAuthorizedToComputeReport",
+	"PricingPolicyNotExists",
+	"ContractIsNotUnique",
+	"NameExists",
+	"NameNotValid",
+	"InvalidContractType",
+	"TFTPriceValueError",
+	"NotEnoughResourcesOnNode",
+	"NodeNotAuthorizedToReportResources",
+	"MethodIsDeprecated",
+	"NodeHasActiveContracts",
+	"NodeHasRentContract",
+	"NodeIsNotDedicated",
+	"NodeNotAvailableToDeploy",
+	"CannotUpdateContractInGraceState",
+	"NumOverflow",
+	"OffchainSignedTxCannotSign",
+	"OffchainSignedTxAlreadySent",
+	"OffchainSignedTxNoLocalAccountAvailable",
+	"NameContractNameTooShort",
+	"NameContractNameTooLong",
+	"InvalidProviderConfiguration",
+	"NoSuchSolutionProvider",
+	"SolutionProviderNotApproved",
+	"TwinNotAuthorized",
+	"ServiceContractNotExists",
+	"ServiceContractCreationNotAllowed",
+	"ServiceContractModificationNotAllowed",
+	"ServiceContractApprovalNotAllowed",
+	"ServiceContractRejectionNotAllowed",
+	"ServiceContractBillingNotApprovedByBoth",
+	"ServiceContractBillingVariableAmountTooHigh",
+	"ServiceContractBillMetadataTooLong",
+	"ServiceContractMetadataTooLong",
+	"ServiceContractNotEnoughFundsToPayBill",
+	"CanOnlyIncreaseFrequency",
+	"IsNotAnAuthority",
+	"WrongAuthority",
+}
+
+// https://github.com/threefoldtech/tfchain/blob/development/substrate-node/pallets/pallet-smart-contract/src/lib.rs#L321
+var tfgridModuleErrors = []string{
+	"NoneValue",
+	"StorageOverflow",
+	"CannotCreateNode",
+	"NodeNotExists",
+	"NodeWithTwinIdExists",
+	"CannotDeleteNode",
+	"NodeDeleteNotAuthorized",
+	"NodeUpdateNotAuthorized",
+	"FarmExists",
+	"FarmNotExists",
+	"CannotCreateFarmWrongTwin",
+	"CannotUpdateFarmWrongTwin",
+	"CannotDeleteFarm",
+	"CannotDeleteFarmWithPublicIPs",
+	"CannotDeleteFarmWithNodesAssigned",
+	"CannotDeleteFarmWrongTwin",
+	"IpExists",
+	"IpNotExists",
+	"EntityWithNameExists",
+	"EntityWithPubkeyExists",
+	"EntityNotExists",
+	"EntitySignatureDoesNotMatch",
+	"EntityWithSignatureAlreadyExists",
+	"CannotUpdateEntity",
+	"CannotDeleteEntity",
+	"SignatureLengthIsIncorrect",
+	"TwinExists",
+	"TwinNotExists",
+	"TwinWithPubkeyExists",
+	"CannotCreateTwin",
+	"UnauthorizedToUpdateTwin",
+	"PricingPolicyExists",
+	"PricingPolicyNotExists",
+	"PricingPolicyWithDifferentIdExists",
+	"CertificationCodeExists",
+	"FarmingPolicyAlreadyExists",
+	"FarmPayoutAdressAlreadyRegistered",
+	"FarmerDoesNotHaveEnoughFunds",
+	"UserDidNotSignTermsAndConditions",
+	"FarmerDidNotSignTermsAndConditions",
+	"FarmerNotAuthorized",
+	"InvalidFarmName",
+	"AlreadyCertifier",
+	"NotCertifier",
+	"NotAllowedToCertifyNode",
+	"FarmingPolicyNotExists",
+	"RelayTooShort",
+	"RelayTooLong",
+	"InvalidRelay",
+	"FarmNameTooShort",
+	"FarmNameTooLong",
+	"InvalidPublicIP",
+	"PublicIPTooShort",
+	"PublicIPTooLong",
+	"GatewayIPTooShort",
+	"GatewayIPTooLong",
+	"IP4TooShort",
+	"IP4TooLong",
+	"InvalidIP4",
+	"GW4TooShort",
+	"GW4TooLong",
+	"InvalidGW4",
+	"IP6TooShort",
+	"IP6TooLong",
+	"InvalidIP6",
+	"GW6TooShort",
+	"GW6TooLong",
+	"InvalidGW6",
+	"DomainTooShort",
+	"DomainTooLong",
+	"InvalidDomain",
+	"MethodIsDeprecated",
+	"InterfaceNameTooShort",
+	"InterfaceNameTooLong",
+	"InvalidInterfaceName",
+	"InterfaceMacTooShort",
+	"InterfaceMacTooLong",
+	"InvalidMacAddress",
+	"InterfaceIpTooShort",
+	"InterfaceIpTooLong",
+	"InvalidInterfaceIP",
+	"InvalidZosVersion",
+	"FarmingPolicyExpired",
+	"InvalidHRUInput",
+	"InvalidSRUInput",
+	"InvalidCRUInput",
+	"InvalidMRUInput",
+	"LatitudeInputTooShort",
+	"LatitudeInputTooLong",
+	"InvalidLatitudeInput",
+	"LongitudeInputTooShort",
+	"LongitudeInputTooLong",
+	"InvalidLongitudeInput",
+	"CountryNameTooShort",
+	"CountryNameTooLong",
+	"InvalidCountryName",
+	"CityNameTooShort",
+	"CityNameTooLong",
+	"InvalidCityName",
+	"InvalidCountryCityPair",
+	"SerialNumberTooShort",
+	"SerialNumberTooLong",
+	"InvalidSerialNumber",
+	"DocumentLinkInputTooShort",
+	"DocumentLinkInputTooLong",
+	"InvalidDocumentLinkInput",
+	"DocumentHashInputTooShort",
+	"DocumentHashInputTooLong",
+	"InvalidDocumentHashInput",
+	"InvalidPublicConfig",
+	"UnauthorizedToChangePowerTarget",
+	"InvalidRelayAddress",
+}
+
+var tftBridgeModuleErrors = []string{
+	"ValidatorExists",
+	"ValidatorNotExists",
+	"TransactionValidatorExists",
+	"TransactionValidatorNotExists",
+	"MintTransactionExists",
+	"MintTransactionAlreadyExecuted",
+	"MintTransactionNotExists",
+	"BurnTransactionExists",
+	"BurnTransactionNotExists",
+	"BurnSignatureExists",
+	"EnoughBurnSignaturesPresent",
+	"RefundSignatureExists",
+	"BurnTransactionAlreadyExecuted",
+	"RefundTransactionNotExists",
+	"RefundTransactionAlreadyExecuted",
+	"EnoughRefundSignaturesPresent",
+	"NotEnoughBalanceToSwap",
+	"AmountIsLessThanWithdrawFee",
+	"AmountIsLessThanDepositFee",
+	"WrongParametersProvided",
+	"InvalidStellarPublicKey",
+}
+
+type CallResponse struct {
+	Hash     types.Hash
+	Events   *EventRecords
+	Block    *types.SignedBlock
+	Identity Identity
+}
+
+// Sign signs data with the private key under the given derivation path, returning the signature. Requires the subkey
+// command to be in path
+func signBytes(data []byte, privateKeyURI string, scheme subkey.Scheme) ([]byte, error) {
+	// if data is longer than 256 bytes, hash it first
+	if len(data) > 256 {
+		h := blake2b.Sum256(data)
+		data = h[:]
+	}
+
+	kyr, err := subkey.DeriveKeyPair(scheme, privateKeyURI)
+	if err != nil {
+		return nil, err
+	}
+
+	signature, err := kyr.Sign(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return signature, nil
+}
+
+// Sign adds a signature to the extrinsic
+func (s *Substrate) sign(e *types.Extrinsic, signer Identity, o types.SignatureOptions) error {
+	if e.Type() != types.ExtrinsicVersion4 {
+		return fmt.Errorf("unsupported extrinsic version: %v (isSigned: %v, type: %v)", e.Version, e.IsSigned(), e.Type())
+	}
+
+	mb, err := types.Encode(e.Method)
+	if err != nil {
+		return err
+	}
+
+	era := o.Era
+	if !o.Era.IsMortalEra {
+		era = types.ExtrinsicEra{IsImmortalEra: true}
+	}
+
+	payload := types.ExtrinsicPayloadV4{
+		ExtrinsicPayloadV3: types.ExtrinsicPayloadV3{
+			Method:      mb,
+			Era:         era,
+			Nonce:       o.Nonce,
+			Tip:         o.Tip,
+			SpecVersion: o.SpecVersion,
+			GenesisHash: o.GenesisHash,
+			BlockHash:   o.BlockHash,
+		},
+		TransactionVersion: o.TransactionVersion,
+	}
+
+	signerPubKey := types.NewMultiAddressFromAccountID(signer.PublicKey())
+
+	b, err := types.Encode(payload)
+	if err != nil {
+		return err
+	}
+
+	sig, err := signer.Sign(b)
+
+	if err != nil {
+		return err
+	}
+	msig := signer.MultiSignature(sig)
+	extSig := types.ExtrinsicSignatureV4{
+		Signer:    signerPubKey,
+		Signature: msig,
+		Era:       era,
+		Nonce:     o.Nonce,
+		Tip:       o.Tip,
+	}
+
+	e.Signature = extSig
+
+	// mark the extrinsic as signed
+	e.Version |= types.ExtrinsicBitSigned
+
+	return nil
+}
+
+// Call call this extrinsic and retry if Usurped
+func (s *Substrate) Call(cl Conn, meta Meta, identity Identity, call types.Call) (response *CallResponse, err error) {
+	for {
+		hash, err := s.CallOnce(cl, meta, identity, call)
+
+		if errors.Is(err, ErrIsUsurped) {
+			continue
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		events, block, err := s.getEventRecords(cl, meta, hash)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error extracting events from block(%s)", hash.Hex())
+		}
+		callResponse := CallResponse{
+			Hash:     hash,
+			Block:    block,
+			Events:   events,
+			Identity: identity,
+		}
+		err = s.checkForError(&callResponse)
+		if err != nil {
+			return nil, err
+		}
+		return &callResponse, err
+	}
+}
+
+func (s *Substrate) CallOnce(cl Conn, meta Meta, identity Identity, call types.Call) (hash types.Hash, err error) {
+	// Create the extrinsic
+	ext := types.NewExtrinsic(call)
+
+	genesisHash, err := cl.RPC.Chain.GetBlockHash(0)
+	if err != nil {
+		return hash, errors.Wrap(err, "failed to get genesisHash")
+	}
+
+	rv, err := cl.RPC.State.GetRuntimeVersionLatest()
+	if err != nil {
+		return hash, err
+	}
+
+	//node.Address =identity.PublicKey
+	account, err := s.getAccount(cl, meta, identity)
+	if err != nil {
+		return hash, errors.Wrap(err, "failed to get account")
+	}
+
+	o := types.SignatureOptions{
+		BlockHash:          genesisHash,
+		Era:                types.ExtrinsicEra{IsMortalEra: false},
+		GenesisHash:        genesisHash,
+		Nonce:              types.NewUCompactFromUInt(uint64(account.Nonce)),
+		SpecVersion:        rv.SpecVersion,
+		Tip:                types.NewUCompactFromUInt(0),
+		TransactionVersion: rv.TransactionVersion,
+	}
+
+	err = s.sign(&ext, identity, o)
+	if err != nil {
+		return hash, errors.Wrap(err, "failed to sign")
+	}
+
+	// Send the extrinsic
+	sub, err := cl.RPC.Author.SubmitAndWatchExtrinsic(ext)
+	if err != nil {
+		return hash, errors.Wrap(err, "failed to submit extrinsic")
+	}
+
+	defer sub.Unsubscribe()
+
+	ch := sub.Chan()
+	ech := sub.Err()
+
+loop:
+	for {
+		select {
+		case err := <-ech:
+			return hash, errors.Wrap(err, "error failed on extrinsic status")
+		case <-time.After(30 * time.Second):
+			return hash, fmt.Errorf("extrinsic timeout waiting for block")
+		case event := <-ch:
+			if event.IsReady || event.IsBroadcast {
+				continue
+			} else if event.IsInBlock {
+				hash = event.AsInBlock
+				break loop
+			} else if event.IsFinalized {
+				// we shouldn't hit this case
+				// any more since InBlock will always
+				// happen first we leave it only
+				// as a safety net
+				hash = event.AsFinalized
+				break loop
+			} else if event.IsDropped || event.IsInvalid {
+				return hash, fmt.Errorf("failed to make call")
+			} else if event.IsUsurped {
+				return hash, ErrIsUsurped
+			} else {
+				log.Error().Err(err).Msgf("extrinsic block in an unhandled state: %+v", event)
+			}
+		}
+	}
+
+	return hash, nil
+}
+
+func (s *Substrate) getEventRecords(cl Conn, meta Meta, blockHash types.Hash) (*EventRecords, *types.SignedBlock, error) {
+	key, err := types.CreateStorageKey(meta, "System", "Events", nil, nil)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to create storage key")
+	}
+
+	raw, err := cl.RPC.State.GetStorageRaw(key, blockHash)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to get raw storage")
+	}
+
+	block, err := cl.RPC.Chain.GetBlock(blockHash)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to get block")
+	}
+
+	events := EventRecords{}
+	err = types.EventRecordsRaw(*raw).DecodeEventRecords(meta, &events)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to decode event")
+	}
+
+	return &events, block, nil
+}
+
+func (s *Substrate) getServiceContractIdsFromEvents(callResponse *CallResponse) ([]uint64, error) {
+	var serviceContractIDs []uint64
+	twinID, err := s.GetTwinByPubKey(callResponse.Identity.PublicKey())
+	if err != nil {
+		return serviceContractIDs, err
+	}
+	if len(callResponse.Events.SmartContractModule_ServiceContractCreated) > 0 {
+		for _, e := range callResponse.Events.SmartContractModule_ServiceContractCreated {
+			if e.ServiceContract.ServiceTwinID == types.U32(twinID) ||
+				e.ServiceContract.ConsumerTwinID == types.U32(twinID) {
+				serviceContractIDs = append(serviceContractIDs, uint64(e.ServiceContract.ServiceContractID))
+			}
+		}
+	}
+
+	return serviceContractIDs, nil
+}
+
+func (s *Substrate) checkForError(callResponse *CallResponse) error {
+	if len(callResponse.Events.System_ExtrinsicFailed) > 0 {
+		for _, e := range callResponse.Events.System_ExtrinsicFailed {
+			who := callResponse.Block.Block.Extrinsics[e.Phase.AsApplyExtrinsic].Signature.Signer.AsID
+			if types.NewAccountID(callResponse.Identity.PublicKey()) == who {
+				if int(e.DispatchError.ModuleError.Index) < len(moduleErrors) {
+					if int(e.DispatchError.ModuleError.Error) >= len(moduleErrors[e.DispatchError.ModuleError.Index]) || moduleErrors[e.DispatchError.ModuleError.Index] == nil {
+						return fmt.Errorf("module error (%d) with unknown code %d occured, please update the module error list", e.DispatchError.ModuleError.Index, e.DispatchError.ModuleError.Error)
+					}
+					return fmt.Errorf(moduleErrors[e.DispatchError.ModuleError.Index][e.DispatchError.ModuleError.Error])
+				} else {
+					return fmt.Errorf("unknown module error (%d) with code %d occured, please create the module error list", e.DispatchError.ModuleError.Index, e.DispatchError.ModuleError.Error)
+				}
+			}
+		}
+	}
+
+	return nil
+}
