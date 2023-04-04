0c72e0854270337f3a30baf58e42306001a5d731
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2023-04-04 15:40:47 +0200

    feat: add clients (#647)

---
+++ clients/tfchain_client_go/transfer.go
@@ -0,0 +1,34 @@
+package substrate
+
+import (
+	"math/big"
+
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/pkg/errors"
+)
+
+func (s *Substrate) Transfer(identity Identity, amount uint64, destination AccountID) error {
+	cl, meta, err := s.GetClient()
+	if err != nil {
+		return err
+	}
+
+	dest := types.NewMultiAddressFromAccountID(destination[:])
+	bal := big.NewInt(int64(amount))
+
+	c, err := types.NewCall(meta, "Balances.transfer", dest, types.NewUCompact(bal))
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		return errors.Wrap(err, "failed to create call")
+	}
+
+	_, err = s.Call(cl, meta, identity, c)
+	if err != nil {
+		return errors.Wrap(err, "failed to transfer")
+	}
+
+	return nil
+}
