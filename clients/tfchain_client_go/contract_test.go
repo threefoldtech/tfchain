7196bb72b91d9ce3ac2ec90a604ad8a241bf88e0
dylanVerstraete <dylan_verstraete1@hotmail.com>
2023-04-12 12:59:25 +0200

    feat: add batch cancel contracts

---
+++ clients/tfchain_client_go/contract_test.go
@@ -0,0 +1,132 @@
+package substrate
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNameContract(t *testing.T) {
+	var contractID uint64
+	var nameContractID uint64
+
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	identity, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	require.NoError(t, err)
+
+	assertCreateFarm(t, cl)
+
+	contractID, err = cl.CreateNameContract(identity, testName)
+	require.NoError(t, err)
+
+	nameContractID, err = cl.GetContractIDByNameRegistration(testName)
+	require.NoError(t, err)
+	require.Equal(t, contractID, nameContractID)
+
+	err = cl.CancelContract(identity, contractID)
+	require.NoError(t, err)
+
+}
+
+func TestNodeContract(t *testing.T) {
+	var nodeID uint32
+	var contractID uint64
+	var contractIDWithHash uint64
+	var contract *Contract
+
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	identity, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	require.NoError(t, err)
+
+	farmID, twinID := assertCreateFarm(t, cl)
+
+	nodeID = assertCreateNode(t, cl, farmID, twinID, identity)
+
+	require.NoError(t, err)
+
+	contractID, err = cl.CreateNodeContract(identity, nodeID, "", "", 0, nil)
+	require.NoError(t, err)
+
+	contract, err = cl.GetContract(contractID)
+	require.NoError(t, err)
+
+	contractIDWithHash, err = cl.GetContractWithHash(uint32(
+		contract.ContractType.NodeContract.Node),
+		contract.ContractType.NodeContract.DeploymentHash)
+
+	require.NoError(t, err)
+	require.Equal(t, contractID, contractIDWithHash)
+
+	err = cl.CancelContract(identity, contractID)
+	require.NoError(t, err)
+}
+
+func TestGetRentContract(t *testing.T) {
+	var nodeID uint32
+	var contractID uint64
+
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	identity, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	require.NoError(t, err)
+
+	farmID, twinID := assertCreateFarm(t, cl)
+
+	nodeID = assertCreateNode(t, cl, farmID, twinID, identity)
+
+	// if node had a previous contact from another test, make sure to cancel it
+	if prev, err := cl.GetNodeRentContract(nodeID); err == nil {
+		err = cl.CancelContract(identity, prev)
+		require.NoError(t, err)
+	}
+
+	contractID, err = cl.CreateRentContract(identity, nodeID, nil)
+	require.NoError(t, err)
+
+	rentContract, err := cl.GetNodeRentContract(nodeID)
+	require.NoError(t, err)
+	require.Equal(t, contractID, rentContract)
+
+	_, err = cl.GetContract(contractID)
+	require.NoError(t, err)
+
+	err = cl.CancelContract(identity, contractID)
+	require.NoError(t, err)
+}
+
+func TestCancelBatch(t *testing.T) {
+	var nodeID uint32
+
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	identity, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	require.NoError(t, err)
+
+	farmID, twinID := assertCreateFarm(t, cl)
+
+	nodeID = assertCreateNode(t, cl, farmID, twinID, identity)
+
+	require.NoError(t, err)
+
+	contractID_1, err := cl.CreateNodeContract(identity, nodeID, "", "adf503a0e016b0a74804c07494f87252", 0, nil)
+	require.NoError(t, err)
+	_, err = cl.GetContract(contractID_1)
+	require.NoError(t, err)
+
+	contractID_2, err := cl.CreateNodeContract(identity, nodeID, "", "adf503a0e016b0a74804c07494f87251", 0, nil)
+	require.NoError(t, err)
+	_, err = cl.GetContract(contractID_2)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second * 10)
+
+	err = cl.BatchCancelContract(identity, []uint64{contractID_1, contractID_2})
+	require.NoError(t, err)
+}
