c996fa19c9e6352b2711982bfad99c2d621cecda
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-05-03 15:24:38 +0300

    feat: add batch create contract

--- clients/tfchain-client-go/contract_test.go
+++ clients/tfchain-client-go/contract_test.go
@@ -127,3 +127,148 @@ func TestCancelBatch(t *testing.T) {
 	err = cl.BatchCancelContract(identity, []uint64{contractID_1, contractID_2})
 	require.NoError(t, err)
 }
+
+func TestCreateBatch(t *testing.T) {
+	var nodeID uint32
+
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	identity, err := NewIdentityFromSr25519Phrase(BobMnemonics)
+	require.NoError(t, err)
+
+	farmID, twinID := assertCreateFarm(t, cl)
+
+	nodeID = assertCreateNode(t, cl, farmID, twinID, identity)
+	require.NoError(t, err)
+
+	t.Run("batch all succeeded", func(t *testing.T) {
+		contracts := []BatchCreateContractData{{
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87255",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "d20454ba65dfd4577af63d142c622cdc",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Name: "test",
+		}}
+
+		c, err := cl.BatchAllCreateContract(identity, contracts)
+		require.NoError(t, err)
+		require.Len(t, c, 3)
+
+		_, err = cl.GetContract(c[0])
+		require.NoError(t, err)
+
+		_, err = cl.GetContract(c[1])
+		require.NoError(t, err)
+
+		_, err = cl.GetContract(c[2])
+		require.NoError(t, err)
+
+		err = cl.BatchCancelContract(identity, c)
+		require.NoError(t, err)
+	})
+	t.Run("batch all failed", func(t *testing.T) {
+		// second contract already exists (same hash)
+		contracts := []BatchCreateContractData{{
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87250",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87250",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Name: "test",
+		}}
+
+		c, err := cl.BatchAllCreateContract(identity, contracts)
+		require.Error(t, err)
+		require.Len(t, c, 0)
+
+		hash := NewHexHash(contracts[0].Hash)
+
+		// first contract should be rolled back
+		_, err = cl.GetContractWithHash(nodeID, hash)
+		require.Error(t, err)
+	})
+	t.Run("batch succeeded", func(t *testing.T) {
+		contracts := []BatchCreateContractData{{
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87251",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "d20454ba65dfd4577af63d142c622cda",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Name: "test",
+		}}
+
+		c, index, err := cl.BatchCreateContract(identity, contracts)
+		require.NoError(t, err)
+		require.Len(t, c, 3)
+		require.Nil(t, index)
+
+		_, err = cl.GetContract(c[0])
+		require.NoError(t, err)
+
+		_, err = cl.GetContract(c[1])
+		require.NoError(t, err)
+
+		_, err = cl.GetContract(c[2])
+		require.NoError(t, err)
+
+		err = cl.BatchCancelContract(identity, c)
+		require.NoError(t, err)
+	})
+
+	t.Run("batch failed", func(t *testing.T) {
+		// second contract already exists (same hash)
+		contracts := []BatchCreateContractData{{
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87253",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Node:               nodeID,
+			Body:               "",
+			Hash:               "adf503a0e0160ba74804c27494f87253",
+			PublicIPs:          0,
+			SolutionProviderID: nil,
+		}, {
+			Name: "test",
+		}}
+
+		c, index, err := cl.BatchCreateContract(identity, contracts)
+		require.Error(t, err)
+		require.NotNil(t, index)
+		require.Equal(t, *index, 1)
+		require.Len(t, c, 1)
+
+		// first contract should be created
+		_, err = cl.GetContract(c[0])
+		require.NoError(t, err)
+
+		// third contract should not be created
+		_, err = cl.GetContractIDByNameRegistration(contracts[2].Name)
+		require.Error(t, err)
+	})
+
+}
