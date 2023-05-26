8cbfb6946b1350b8c83a38bfd107f67f03d4eef3
renauter <renauter@gmail.com>
2023-05-26 01:18:26 -0300

    migration wip

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -766,7 +766,10 @@ pub type Executive = frame_executive::Executive<
 
 // All migrations executed on runtime upgrade as a nested tuple of types implementing
 // `OnRuntimeUpgrade`.
-type Migrations = (pallet_smart_contract::migrations::v9::CleanStorageState<Runtime>,);
+type Migrations = (
+    pallet_smart_contract::migrations::v9::CleanStorageState<Runtime>,
+    pallet_smart_contract::migrations::v10::ReworkBillingLoopInsertion<Runtime>,
+);
 
 // follows Substrate's non destructive way of eliminating  otherwise required
 // repetion: https://github.com/paritytech/substrate/pull/10592
