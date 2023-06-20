50613c54630268c8682ca71e1621994409b4f3e5
sameh-farouk <samehabouelsaad@gmail.com>
2023-06-20 12:48:32 +0300

    style: apply formatting

--- substrate-node/pallets/pallet-kvstore/src/kvstore.rs
+++ substrate-node/pallets/pallet-kvstore/src/kvstore.rs
@@ -1,26 +1,18 @@
-use sp_std::prelude::{Vec};
-use frame_support::pallet_prelude::{DispatchResultWithPostInfo, ensure};
-use super::pallet::{Config, Pallet, TFKVStore, Event, Error};
+use super::pallet::{Config, Error, Event, Pallet, TFKVStore};
+use frame_support::pallet_prelude::{ensure, DispatchResultWithPostInfo};
+use sp_std::prelude::Vec;
 
 impl<T: Config> Pallet<T> {
-    pub fn _set(
-        user: T::AccountId,
-        key: Vec<u8>,
-        value: Vec<u8>,
-    ) -> DispatchResultWithPostInfo {
+    pub fn _set(user: T::AccountId, key: Vec<u8>, value: Vec<u8>) -> DispatchResultWithPostInfo {
         ensure!(key.len() <= 512, Error::<T>::KeyIsTooLarge);
         ensure!(value.len() <= 2048, Error::<T>::ValueIsTooLarge);
-            
+
         <TFKVStore<T>>::insert(&user, &key, &value);
         Self::deposit_event(Event::EntrySet(user, key, value));
         Ok(().into())
     }
 
-    pub fn _delete(
-        user: T::AccountId,
-        key: Vec<u8>,
-    ) -> DispatchResultWithPostInfo {
-
+    pub fn _delete(user: T::AccountId, key: Vec<u8>) -> DispatchResultWithPostInfo {
         ensure!(
             <TFKVStore<T>>::contains_key(&user, &key),
             Error::<T>::NoValueStored
