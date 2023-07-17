33d271d77ecf17d5bc9aa49aa66a8b4cecc1daee
rawdaGastan <rody.rawda.rr@gmail.com>
2023-07-17 13:47:31 +0300

    add tft price and pricing policies

---
+++ clients/tfchain-client-go/price_test.go
@@ -0,0 +1,21 @@
+package substrate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPrice(t *testing.T) {
+	cl := startLocalConnection(t)
+	defer cl.Close()
+
+	price, err := cl.GetTFTPrice()
+	require.Greater(t, int(price), 0)
+	require.NoError(t, err)
+
+	pricingPolicy, err := cl.GetPricingPolicies(1)
+	require.Equal(t, pricingPolicy.Name, "threefold_default_pricing_policy")
+	require.Equal(t, int(pricingPolicy.ID), 1)
+	require.NoError(t, err)
+}
