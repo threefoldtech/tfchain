cb3037978abc2f2858d541d1364aba5323868615
rawdaGastan <rody.rawda.rr@gmail.com>
2023-07-18 12:01:00 +0300

    swap checks in test

--- clients/tfchain-client-go/price_test.go
+++ clients/tfchain-client-go/price_test.go
@@ -11,15 +11,15 @@ func TestPrice(t *testing.T) {
 	defer cl.Close()
 
 	price, err := cl.GetTFTPrice()
-	require.Greater(t, int(price), 0)
 	require.NoError(t, err)
+	require.Greater(t, int(price), 0)
 
 	price, err = cl.GetAverageTFTPrice()
-	require.Greater(t, int(price), 0)
 	require.NoError(t, err)
+	require.Greater(t, int(price), 0)
 
 	pricingPolicy, err := cl.GetPricingPolicy(1)
+	require.NoError(t, err)
 	require.Equal(t, pricingPolicy.Name, "threefold_default_pricing_policy")
 	require.Equal(t, int(pricingPolicy.ID), 1)
-	require.NoError(t, err)
 }
