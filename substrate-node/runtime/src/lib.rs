a920b0f5d3deb2f91dc33b31bf509bd8d1b6bf4b
Erwan Renaut <73958772+renauter@users.noreply.github.com>
2023-10-04 10:06:31 -0300

    chore: bump to version 2.6.0-rc1 (specVersion: 146) (#872)

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("substrate-threefold"),
     impl_name: create_runtime_str!("substrate-threefold"),
     authoring_version: 1,
-    spec_version: 145,
+    spec_version: 146,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
@@ -770,13 +770,7 @@ pub type Executive = frame_executive::Executive<
 
 // All migrations executed on runtime upgrade as a nested tuple of types implementing
 // `OnRuntimeUpgrade`.
-type Migrations = (
-    // pallet_tfgrid::migrations::v16::KillNodeGpuStatus<Runtime>,
-    // pallet_smart_contract::migrations::v10::ReworkBillingLoopInsertion<Runtime>,
-    // pallet_smart_contract::migrations::v11::ExtendContractLock<Runtime>,
-    // migrations::remove_sudo::RemoveSudo<Runtime>,
-    pallet_tfgrid::migrations::v17::FixFarmPublicIps<Runtime>,
-);
+type Migrations = (pallet_tfgrid::migrations::v17::FixFarmPublicIps<Runtime>,);
 
 // follows Substrate's non destructive way of eliminating  otherwise required
 // repetion: https://github.com/paritytech/substrate/pull/10592
