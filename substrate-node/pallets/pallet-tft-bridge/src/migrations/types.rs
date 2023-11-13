efb6e075be6b74e7b13c95cadcbb98dc7b861967
sameh-farouk <samehabouelsaad@gmail.com>
2023-11-13 04:12:34 +0200

    test: fix try-runtime pre and post hooks

--- substrate-node/pallets/pallet-tft-bridge/src/migrations/types.rs
+++ substrate-node/pallets/pallet-tft-bridge/src/migrations/types.rs
@@ -1,7 +1,9 @@
 pub mod v1 {
-    use crate::types::StellarSignature;
+    use frame_support::{storage_alias, StorageMap, Blake2_128Concat, pallet_prelude::OptionQuery};
     use parity_scale_codec::{Decode, Encode};
     use scale_info::{TypeInfo, prelude::vec::Vec};
+
+    use crate::{Config, Pallet, types::StellarSignature};
 
     #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, Debug, TypeInfo)]
     pub struct BurnTransaction<BlockNumber> {
@@ -11,4 +13,15 @@ pub mod v1 {
         pub signatures: Vec<StellarSignature>,
         pub sequence_number: u64,
     }
+
+    #[cfg(feature = "try-runtime")]
+    #[storage_alias]    
+    pub type BurnTransactions<T: Config> =
+    StorageMap<Pallet<T>, Blake2_128Concat, u64, BurnTransaction<<T as frame_system::Config>::BlockNumber>, OptionQuery>;
+
+    #[cfg(feature = "try-runtime")]
+    #[storage_alias]  
+    pub type ExecutedBurnTransactions<T: Config> =
+        StorageMap<Pallet<T>, Blake2_128Concat, u64, BurnTransaction<<T as frame_system::Config>::BlockNumber>, OptionQuery>;
+
 }
