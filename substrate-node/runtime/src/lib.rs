3b5bae20befd10c848d5a77abb5c72f7be928ef8
Erwan Renaut <73958772+renauter@users.noreply.github.com>
2023-12-06 15:49:35 -0300

    chore: bump to version 2.7.0-rc1 (specVersion: 147) (#910)

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("substrate-threefold"),
     impl_name: create_runtime_str!("substrate-threefold"),
     authoring_version: 1,
-    spec_version: 146,
+    spec_version: 147,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
@@ -774,7 +774,6 @@ pub type Executive = frame_executive::Executive<
 // All migrations executed on runtime upgrade as a nested tuple of types implementing
 // `OnRuntimeUpgrade`.
 type Migrations = (
-    pallet_tfgrid::migrations::v17::FixFarmPublicIps<Runtime>,
     pallet_tft_bridge::migrations::v2::MigrateBurnTransactionsV2<Runtime>,
     pallet_scheduler::migration::v3::MigrateToV4<Runtime>,
     migrations::update_storage_version::PalletBalancesToV1<Runtime>,
