ea18a6e124561119e3bcafe628750718e6513224
Erwan Renaut <73958772+renauter@users.noreply.github.com>
2024-01-24 13:20:57 -0300

    chore: bump to version 2.7.0-rc4 (specVersion: 149) (#949)

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("substrate-threefold"),
     impl_name: create_runtime_str!("substrate-threefold"),
     authoring_version: 1,
-    spec_version: 148,
+    spec_version: 149,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
