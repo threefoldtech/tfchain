ceff3a21e6d5c90a71e297b7e3c47db7b6bcb918
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2024-10-01 00:04:19 +0300

    chore: fix typos

--- substrate-node/pallets/pallet-smart-contract/src/tests.rs
+++ substrate-node/pallets/pallet-smart-contract/src/tests.rs
@@ -3184,8 +3184,8 @@ fn test_node_contract_with_overdraft_on_dedicated_node_shouldnt_be_restored_when
             .should_call_bill_contract(node_contract_id, Ok(Pays::Yes.into()), 12);
         run_to_block(12, Some(&mut pool_state));
 
-        let node_contract = SmartContractModule::contracts(rent_contract_id).unwrap();
-        assert_eq!(node_contract.state, types::ContractState::GracePeriod(11));
+        let rent_contract = SmartContractModule::contracts(rent_contract_id).unwrap();
+        assert_eq!(rent_contract.state, types::ContractState::GracePeriod(11));
 
         let node_contract = SmartContractModule::contracts(node_contract_id).unwrap();
         assert_eq!(node_contract.state, types::ContractState::GracePeriod(11));
@@ -3206,8 +3206,8 @@ fn test_node_contract_with_overdraft_on_dedicated_node_shouldnt_be_restored_when
             .should_call_bill_contract(rent_contract_id, Ok(Pays::Yes.into()), 21);
         run_to_block(21, Some(&mut pool_state));
 
-        let node_contract = SmartContractModule::contracts(rent_contract_id).unwrap();
-        assert_eq!(node_contract.state, types::ContractState::Created);
+        let rent_contract = SmartContractModule::contracts(rent_contract_id).unwrap();
+        assert_eq!(rent_contract.state, types::ContractState::Created);
 
         let node_contract = SmartContractModule::contracts(node_contract_id).unwrap();
         assert_eq!(node_contract.state, types::ContractState::GracePeriod(11));
@@ -3310,7 +3310,7 @@ fn test_rent_contract_should_wait_for_proper_distribution_of_rewards_when_grace_
             );
             run_to_block(block_number+1, Some(&mut pool_state));
         }
-        // Check if the events contains RewardDistributed event
+        // Check if the events contain RewardDistributed event
         let events = System::events();
         log::debug!("Events: {:?}", events);
         assert_eq!(
