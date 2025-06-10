a309d61ecb30c1626c69247a60a8e78de0fe4a65
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-06-10 16:58:15 +0300

    Should not allow minting if the sender account is the bridge account

--- bridge/tfchain_bridge/pkg/bridge/mint.go
+++ bridge/tfchain_bridge/pkg/bridge/mint.go
@@ -19,6 +19,23 @@ import (
 func (bridge *Bridge) mint(ctx context.Context, senders map[string]*big.Int, tx hProtocol.Transaction) error {
 	logger := log.Logger.With().Str("trace_id", tx.ID).Logger()
 
+	filteredSenders := make(map[string]*big.Int)
+	for sender := range senders {
+		if sender == bridge.wallet.GetKeypair().Address() {
+			logger.Info().
+				Str("event_action", "mint_rejected").
+				Str("event_kind", "event").
+				Str("category", "mint").
+				Dict("metadata", zerolog.Dict().
+					Str("reason", "sender is bridge account").
+					Str("sender", sender)).
+				Msg("rejecting mint from bridge account to prevent circular transactions")
+		} else {
+			filteredSenders[sender] = senders[sender]
+		}
+	}
+
+	senders = filteredSenders
 	minted, err := bridge.subClient.IsMintedAlready(tx.Hash)
 	if err != nil {
 		if !errors.Is(err, substrate.ErrMintTransactionNotFound) {
