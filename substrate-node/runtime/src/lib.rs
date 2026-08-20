e0ec88aa7dce3ce2e39b27c4066b0e8798a8da05
Sameh Abouelsaad <samehabouelsaad@gmail.com>
2026-08-20 14:35:50 +0300

    Bump version to 2.14.0 (spec v159)

--- substrate-node/runtime/src/lib.rs
+++ substrate-node/runtime/src/lib.rs
@@ -154,7 +154,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("substrate-threefold"),
     impl_name: create_runtime_str!("substrate-threefold"),
     authoring_version: 1,
-    spec_version: 158,
+    spec_version: 159,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
